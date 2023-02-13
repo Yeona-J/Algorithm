d82026642355683ad4f4fc4acbb4fc9da599dd0e
Yeona Jeong <yeon.aa.j@gmail.com>
2023-02-13 21:46:11 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,9 +1,14 @@
-from itertools import combinations
-
 heights = [int(input()) for _ in range(9)]
+heights.sort()
+tot = sum(heights)
 
-for combi in combinations(heights, 7):
-    if sum(combi) == 100:
-        for heights in sorted(combi):
-            print(heights)
-        break
+def f():
+    for i in range(8): # ?? 9로하면 안된다는데 이해안감
+        for j in range(i+1, 9):
+            if tot - heights[i] - heights[j] == 100: # 이 if문에 들어오게되면 출력 후 끝(return)
+                for k in range(9):
+                    if i != k and j != k:
+                        print(heights[k])
+                return
+
+f()
